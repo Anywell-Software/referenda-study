5a1e357ffa119ca14d16835342a41a584b6d635f
Shansai Muraleetharan <shansai@muraleetharan.ch>
2026-01-15 06:51:21 +0100

    referenda data is being plotted

--- average-voting-share.R
+++ average-voting-share.R
@@ -17,6 +17,31 @@ for (iter in resources) {
   download.file(iter, fileName)
 }
 
-for (iter in urls) {
-  print(iter$schweiz$vorlagen$result$stimmbeteiligungInProzent)
+files <- list.files(path = "/Users/shansai/Swiss-Referenda/vorlagen", pattern = "\\.json$", full.names = TRUE)
+properties_list <- list()
+
+for (i in 1:length(files)) {
+  content <- fromJSON(files[i])
+  stimmbeteiligung <- content$schweiz$vorlagen$resultat$stimmbeteiligungInProzent
+  
+  properties_list[[i]] <- list(
+    abstimmtag = content$abstimmtag,
+    stimmbeteiligung = mean(stimmbeteiligung, na.rm = TRUE)
+  )
+  length(content$schweiz$vorlagen$resultat$stimmbeteiligungInProzent)
+  print(mean(stimmbeteiligung, na.rm=TRUE))
 }
+
+df <- as.data.frame(do.call(rbind, properties_list))
+df$abstimmtag <- as.Date(unlist(df$abstimmtag), format = "%Y%m%d")
+df$stimmbeteiligung <- as.numeric(unlist(df$stimmbeteiligung))
+print(df)
+df <- df[order(df$abstimmtag), ]
+plot(df$abstimmtag, df$stimmbeteiligung, 
+     type = "b",
+     xlab = "Voting Date",
+     ylab = "Average Turnout (%)",
+     main = "Swiss Referendum Voter Turnout Over Time",
+     col = "blue",
+     pch = 16)
+
